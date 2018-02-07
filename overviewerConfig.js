0118c0f63364a44d0de029196c033791c6ea50e2
Tyler Nakamura <tyler.c.nakamura@gmail.com>
2018-02-07 22:32:49 +0000

    initial commit

---
+++ overviewerConfig.js
@@ -0,0 +1,87 @@
+var overviewerConfig = {
+    "worlds": [
+        "myworld - overworld"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                133, 
+                80, 
+                196
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1517778564, 
+            "name": "Day", 
+            "poititle": "Markers", 
+            "north_direction": 3, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 7, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 3, 
+            "world": "myworld - overworld", 
+            "maxZoom": 7, 
+            "path": "daytime", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                133, 
+                80, 
+                196
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1517778564, 
+            "name": "Night", 
+            "poititle": "Markers", 
+            "north_direction": 3, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 7, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 3, 
+            "world": "myworld - overworld", 
+            "maxZoom": 7, 
+            "path": "nighttime", 
+            "showlocationmarker": true
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "signpost.png", 
+            "queryMarker": "https://google-maps-icons.googlecode.com/files/regroup.png", 
+            "signMarker": "signpost_icon.png", 
+            "spawnMarker": "https://google-maps-icons.googlecode.com/files/home.png", 
+            "bedMarker": "bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1517869431"
+    }
+};
